65289cd0024d0defa48e022142f1c9867b041d67
Addhay (081) <addhaydebnath@gmail.com>
2026-05-03 21:00:49 +0600

    Refactor main.dart by removing unused code
    Removed commented-out code for MyApp and updated imports.

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,4 @@
-/*import 'package:flutter/material.dart';
-import 'package:food_delevery_app/foodlending_page.dart';
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      // theme: ThemeData.dark(),
-      home: FoodLandingPage(),
-    );
-  }
-}*/
 import 'package:flutter/material.dart';
 import 'package:food_delevery_app/login_page.dart';
 
